c8a0103a4c023ddcd76f1e61b15bd983a7ed66b6
somename <someemail>
2015-10-09 12:58:47 +0300

    besouro automatic message

--- src/org/unioulu/tol/sqatlab/gameoflife/Grid.java
+++ src/org/unioulu/tol/sqatlab/gameoflife/Grid.java
@@ -1,19 +1,26 @@
 package org.unioulu.tol.sqatlab.gameoflife;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Grid {
 
+	private Set<Cell> cells = new HashSet<>();
+	
 	public Grid(int size){
 		
 		
 	}
 
 	public void addCell(Cell cell) {
-		// TODO Auto-generated method stub
 		
+		cells.add(cell);
 	}
 
 	public Object getCellsNeighbors(int i, int j) {
-		// TODO Auto-generated method stub
+		
+		
+		
 		return 0; 
 	}
 	
