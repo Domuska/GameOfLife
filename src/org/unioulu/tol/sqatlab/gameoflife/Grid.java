8691808197203aaba8235308628627e8e010e213
somename <someemail>
2015-10-09 12:36:30 +0300

    besouro automatic message

--- src/org/unioulu/tol/sqatlab/gameoflife/Grid.java
+++ src/org/unioulu/tol/sqatlab/gameoflife/Grid.java
@@ -2,4 +2,19 @@ package org.unioulu.tol.sqatlab.gameoflife;
 
 public class Grid {
 
+	public Grid(int size){
+		
+		
+	}
+
+	public void addCell(int i, int j) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public Object getCellsNeighbors(int i, int j) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
 }
