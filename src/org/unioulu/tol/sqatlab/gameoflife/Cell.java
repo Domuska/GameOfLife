fc2996c43bfe1202d13265665dd9b2c19e68ed60
somename <someemail>
2015-10-09 12:19:17 +0300

    besouro automatic message

--- src/org/unioulu/tol/sqatlab/gameoflife/Cell.java
+++ src/org/unioulu/tol/sqatlab/gameoflife/Cell.java
@@ -2,4 +2,17 @@ package org.unioulu.tol.sqatlab.gameoflife;
 
 public class Cell {
 
+	public Cell(String liveliness){		
+		
+	}
+
+	public void nextIteration() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public String getState() {
+		// TODO Auto-generated method stub
+		return "Dead";
+	}
 }
