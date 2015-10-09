42a2be20d1035a276fc9f0e2450ff4c05d62592b
somename <someemail>
2015-10-09 13:11:57 +0300

    besouro automatic message

--- src/org/unioulu/tol/sqatlab/gameoflife/Grid.java
+++ src/org/unioulu/tol/sqatlab/gameoflife/Grid.java
@@ -17,9 +17,21 @@ public class Grid {
 		cells.add(cell);
 	}
 
-	public Object getCellsNeighbors(Cell cell) {
+	public Object getNumberOfCellsNeighbors(Cell cell) {
 		
+		Set<Cell> neighbors = new HashSet<Cell>();
 		
+		int numberOfNeighbors = 0;
+		
+		for(int i = -1; i <= 1 ; i++){
+			
+			Cell neighbor = new Cell(cell.getX(), cell.getY() +1);
+			
+			if(cells.contains(neighbor)){
+				
+				numberOfNeighbors ++;
+			}
+		}
 		
 		return 0; 
 	}
